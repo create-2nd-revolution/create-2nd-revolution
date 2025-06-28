877541111f2927fa4ff08278b0ab6fa28093d54a
virgile <virgile.schwey@outlook.fr>
2025-06-28 13:04:50 +0200

    Mod template

---
+++ src/main/java/com/secondrevolution/secondrevolution/Create2ndRevolutionClient.java
@@ -0,0 +1,18 @@
+package com.secondrevolution.secondrevolution;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = Create2ndRevolution.MODID, dist = Dist.CLIENT)
+public class Create2ndRevolutionClient {
+    public Create2ndRevolutionClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+}
